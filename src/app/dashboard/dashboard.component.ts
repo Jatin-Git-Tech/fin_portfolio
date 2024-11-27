d6a59974b40898259c4b7cc094335eff7c735acb
w138267 <jatin.gupta@worldline.com>
2024-11-27 08:20:23 +0530

    <Fin-app> - Jatin - Fetch market trends data from outside via subscription, interceptor

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { EChartsOption } from 'echarts';
 import { Chart, registerables } from 'chart.js';
 import { AssetDataService } from '../../assetData.service';
+import { HttpClient } from '@angular/common/http';
 Chart.register(...registerables);
 
 @Component({
@@ -14,23 +15,39 @@ Chart.register(...registerables);
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
-  constructor(private sharedService: AssetDataService) {}
+  constructor(private http: HttpClient, private sharedService: AssetDataService) {}
 
- chartOption: EChartsOption = {
-    xAxis: {
-      type: 'category',
-      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-    },
-    yAxis: {
-      type: 'value',
-    },
-    series: [
-      {
-        data: [820, 932, 901, 934, 1290, 1330, 1320],
-        type: 'line',
+//  chartOption: EChartsOption = {
+//     xAxis: {
+//       type: 'category',
+//       data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+//     },
+//     yAxis: {
+//       type: 'value',
+//     },
+//     series: [
+//       {
+//         data: [820, 932, 901, 934, 1290, 1330, 1320],
+//         type: 'line',
+//       },
+//     ],
+//   };
+
+  chartOption: EChartsOption = {
+      xAxis: {
+        type: 'category',
+        data: [], // Dates from JSON data
       },
-    ],
-  };
+      yAxis: {
+        type: 'value',
+      },
+      series: [
+        {
+          data: [], // Closing prices from JSON data
+          type: 'line',
+        },
+      ],
+    };
 
   assetAllocationOptions: EChartsOption = {
     title: {
@@ -49,7 +66,7 @@ export class DashboardComponent implements OnInit {
           { value: 10, name: 'Logistics' },
           { value: 30, name: 'Infrastructure' },
           { value: 10, name: 'Pharma' },
-          { value: 30, name: 'Banking' },
+          { value: 30, name: 'Bankin  g' },
           { value: 10, name: 'Insurance' },
           { value: 10, name: 'Real Estate' },
         ],
@@ -66,11 +83,36 @@ export class DashboardComponent implements OnInit {
 
 
   ngOnInit() {
-    this.sharedService.assetData$.subscribe((data) => {
+
+   // Fetch market trends data
+      this.fetchMarketTrendsData();
+      this.sharedService.assetData$.subscribe((data) => {
       this.assetAllocationOptions = { ...this.assetAllocationOptions,
-
-
       };
+    });
+  }
+
+  private fetchMarketTrendsData() {
+    this.http.get<any>('/assets/marketData.json').subscribe((data: { [x: string]: any; }) => {
+      const marketData = data['Monthly Time Series'];
+      const dates = Object.keys(marketData).slice(0, 6).reverse(); // Last 6 months
+      const closingPrices = dates.map((date) => parseFloat(marketData[date]['4. close']));
+
+      // Update chartOption dynamically
+      this.chartOption = {
+        ...this.chartOption,
+        xAxis: {
+          type: 'category', // Ensure this is a category axis
+          data: dates,
+        },
+        series: [
+          {
+            data: closingPrices, // Assign parsed closing prices
+            type: 'line',
+          },
+        ],
+      };
+      console.log(JSON.stringify(dates));
     });
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { provideRouter, RouterModule, Routes } from '@angular/router';
 import { importProvidersFrom } from '@angular/core';
 import { DashboardComponent } from './app/dashboard/dashboard.component';
 import { FormComponent } from './app/form/form.component';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { httpInterceptor } from './app/http-interceptor.service';
 
 
 const routes: Routes = [
@@ -17,10 +19,13 @@ bootstrapApplication(AppComponent, {
   providers: [
     provideEcharts(), // Register the ECharts library
     provideRouter([
-      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
       { path: 'dashboard', loadComponent: () => import('./app/dashboard/dashboard.component').then(m => m.DashboardComponent) },
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
       { path: 'form', loadComponent: () => import('./app/form/form.component').then(m => m.FormComponent) },
     ]),
+    provideHttpClient(
+      withInterceptors([httpInterceptor]) // Register the interceptor
+    ),
     importProvidersFrom(RouterModule)
   ],
 });
